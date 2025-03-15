d4dd7992cbfc18ecfa2e776d79c396805a838360
RATIONAL331 <rational331@gmail.com>
2025-03-15 20:19:40 +0900

    08-01. Cloud Config

---
+++ IConfigService/src/main/java/com/food/iconfigservice/IConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.food.iconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class IConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IConfigServiceApplication.class, args);
+	}
+
+}
---
+++ IConfigService/src/test/java/com/food/iconfigservice/IConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.iconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
